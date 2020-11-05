cbdc9bb310a4c98c2c384e2cb63ea95fae9664b6
armand <armand.fargeon@gmail.com>
2020-11-05 04:42:08 +0100

    calc2 ok

--- code2.c
+++ code2.c
@@ -31,5 +31,122 @@
 //			This version produces code for a stack machine
 // ---------------------------------------------------------------------
 void produce_code(ast_node *n) {
-  printf("Produire du code pour n = %p!!!!!\n", n);
+  //  printf("Produire du code pour n = %p!!!!!\n", n);
+    switch (AST_KIND(n)) {
+        case k_number:
+            PROD1F("push",  NUMBER_VALUE(n));
+            return;
+        case k_ident:
+            PROD1S("load", VAR_NAME(n));
+            return;
+        case k_operator: {
+            ast_node **op = OPER_OPERANDS(n);
+            int arity = OPER_ARITY(n);
+            int i = 0;
+            switch (OPER_OPERATOR(n)) {
+                case EQ:
+                    produce_code(op[0]);
+                    produce_code(op[1]);
+                    PROD0("cmpeq");
+                    break;
+                case NE:
+                    produce_code(op[0]);
+                    produce_code(op[1]);
+                    PROD0("cmpne");
+                    break;
+                case LE:
+                    produce_code(op[0]);
+                    produce_code(op[1]);
+                    PROD0("cmple");
+                    break;
+                case GE:
+                    produce_code(op[0]);
+                    produce_code(op[1]);
+                    PROD0("cmpge");
+                    break;
+                case '>':
+                    produce_code(op[0]);
+                    produce_code(op[1]);
+                    PROD0("cmpgt");
+                    break;
+                case '<':
+                    produce_code(op[0]);
+                    produce_code(op[1]);
+                    PROD0("cmplt");
+                    break;
+                case '=':
+                    produce_code(op[1]);
+                    PROD1S("store",VAR_NAME(op[0]));
+                    break;
+
+                case '+':
+                    produce_code(op[0]);
+                    produce_code(op[1]);
+                    PROD0("add");
+                    break;
+                case '-':
+                    produce_code(op[0]);
+                    produce_code(op[1]);
+                    PROD0("sub");
+                    break;
+
+                case '*':
+                    produce_code(op[0]);
+                    produce_code(op[1]);
+                    PROD0("mul");
+                    break;
+
+                case '/':
+                    produce_code(op[0]);
+                    produce_code(op[1]);
+                    PROD0("div");
+                    break;
+
+                case KIF:
+                    produce_code(op[0]);
+                    PROD1L("jumpz", i);
+                    produce_code(op[1]);
+                    if(arity == 3) {
+                        PROD1L("jump", ++i);
+                        LABEL(i-1);
+                        produce_code(op[2]);
+                    }
+                    LABEL(i);
+                    break;
+
+                case KELSE:
+                    produce_code(op[0]);
+                    break;
+
+                case KWHILE:
+                    LABEL(i);
+                    produce_code(op[0]);
+                    //if condition is false, jump to the end
+                    PROD1L("jumpz", ++i);
+                    //expr inside the while
+                    produce_code(op[1]);
+                    //back to the while label
+                    PROD1L("jump", i-1);
+                    LABEL(i);
+                    break;
+
+                case KPRINT:
+                    produce_code(op[0]);
+                    PROD0("print");
+                    break;
+
+                case ';':
+                    if (arity == 0) break;
+                    else {
+                        produce_code(op[0]);
+                        produce_code(op[1]);
+                    }
+
+
+            }
+
+        }
+
+    }
+
 }
